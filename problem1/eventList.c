80986bf9b714fd9827b68327fba5ce89b71d6cd5
Phentecost12 <93055187+Phentecost12@users.noreply.github.com>
2022-09-25 09:12:24 -0500

    Lista y añadir evento
    Se implemento la funcion de crear la lista y de añadir eventos a ella

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -1,33 +1,59 @@
 #include "eventList.h"
+#include "event.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+#ifdef DOLOG
+#define LOG(...) fprintf(log,__VA_ARGS__);
+#else
+#define LOG(...) 
+#endif
+
 EventList *CreateEventList(void)
 {
-
+    Event *EventoCreado = (Event*) malloc(sizeof(Event));
+    strcpy(EventoCreado->eventName,"Evento Inicio");
+    EventoCreado->next = NULL;
+    int numero = 0;
+    struct _eventList *Lista = malloc(sizeof(struct _eventList));
+    Lista->isEmpty = numero;
+    Lista->head = EventoCreado;
+    Lista->last = EventoCreado;
+    return Lista;
 }
 
 void DestroyEventList(EventList *this)
 {
+
+
 
 }
 
 Event *SearchEvent(EventList *this, char *name)
 {
 
+
+
 }
 
 void AddEvent(EventList *this, Event *event)
 {
-
+    struct _event *SiguenteEvento = this->head;
+    event->next = SiguenteEvento;
+    this->head = event;
 }
 
 void RemoveEvent(EventList *this, char *name)
 {
+
+
 }
 
 void ListEvents(EventList *this)
 {
 
+ 
+    
+    
 }
