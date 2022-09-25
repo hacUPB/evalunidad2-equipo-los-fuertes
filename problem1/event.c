450321741944875b937b8e1f4a3d04e66b07f513
Phentecost12 <93055187+Phentecost12@users.noreply.github.com>
2022-09-25 09:11:11 -0500

    Eventos
    Se implemento la funcion de crear y destruir eventos

--- problem1/event.c
+++ problem1/event.c
@@ -1,13 +1,22 @@
 #include "event.h"
+#include "eventList.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 
 Event *CreateEvent(char *name)
 {
+
+    struct _event *EventoCreado = (struct _event*) malloc(sizeof(struct _event));
+    strcpy(EventoCreado->eventName,name);
+    EventoCreado->next = NULL;
+    return EventoCreado;
 
 }
 
 void DestroyEvent(Event *this)
 {
-
+    free(this);
 }
+
